3570d409a4c1576e639aa2ec445035244d948415
Victor Salvaya <victorsalvaya@hotmail.com>
2020-07-09 13:36:48 -0300

    Updates

---
+++ precache-manifest.37420cb4b9ddea8b9dc7f717208b92a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744fe4304cc415e678eff1f07519eecb",
+    "url": "/react-star-wars/index.html"
+  },
+  {
+    "revision": "d6290816fa990d0ec571",
+    "url": "/react-star-wars/static/css/main.94a0ed05.chunk.css"
+  },
+  {
+    "revision": "a49df7f8611fdc744bbe",
+    "url": "/react-star-wars/static/js/2.33ee28ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-star-wars/static/js/2.33ee28ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6290816fa990d0ec571",
+    "url": "/react-star-wars/static/js/main.1b1aa5f9.chunk.js"
+  },
+  {
+    "revision": "3327a1f458032ab715dc",
+    "url": "/react-star-wars/static/js/runtime-main.37719f6b.js"
+  },
+  {
+    "revision": "5d1001864d6c0efe802f0964d8b54325",
+    "url": "/react-star-wars/static/media/StarWars-logo.5d100186.png"
+  },
+  {
+    "revision": "0fd3fcc6ba35a9015aa7f0017d96a079",
+    "url": "/react-star-wars/static/media/background4.0fd3fcc6.gif"
+  },
+  {
+    "revision": "b32ab7402b3279c29473980e0021814b",
+    "url": "/react-star-wars/static/media/loading-teste.b32ab740.gif"
+  }
+]);
